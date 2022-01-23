3145ae74ef87358c9ad112695dcd5e54236a0f28
soldierxue <sys@soldierxue.cn>
2022-01-23 16:25:51 +0800

    remove comment

--- queryEC2.sh
+++ queryEC2.sh
@@ -32,11 +32,3 @@ echo ".....Querying the EIP for the ec2 instances"
 aws ec2 describe-addresses --filters $eipFilter --query 'Addresses[*].[InstanceId,PublicIp,PrivateIpAddress]' --output text
 
 echo "Done"
-
-# The launched Instance Id of genshin.1.internal.com is : i-0e378467702af9350
-# The launched Instance Id of genshin.2.internal.com is : i-03d5840d419b0abaa
-# The launched Instance Id of genshin.3.internal.com is : i-06d2bc6f5f46fcb6b
-# The launched Instance Id of genshin.4.internal.com is : i-05d4da2e4416fb49e
-
-
-# aws ec2 describe-addresses --filters "Name=instance-id,Values=[i-0576a33832a46775b,i-00160e24beb22189a]" --query 'Addresses[*].[InstanceId,PublicIp,PrivateIpAddress]' --output text 
